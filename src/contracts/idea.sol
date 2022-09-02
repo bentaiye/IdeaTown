bd2d9ac6bc2096cb59479a33343161056b5a34f9
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-09-02 18:03:32 +0400

    Bug Fixes and some improvements

--- src/contracts/idea.sol
+++ src/contracts/idea.sol
@@ -3,107 +3,150 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
-  function transfer(address, uint256) external returns (bool);
-  function approve(address, uint256) external returns (bool);
-  function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address) external view returns (uint256);
-  function allowance(address, address) external view returns (uint256);
+    function transfer(address, uint256) external returns (bool);
 
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 contract IdeaTown {
-
     uint internal ideasLength = 0;
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    address internal cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Idea {
         address payable owner;
         string name;
         string description;
-        uint perfect;
-        uint good;
-        uint bad;
+        uint rate;
         uint noOfsupports;
+        uint donationAmount;
     }
 
-    mapping (uint => Idea) internal ideas;
+    mapping(uint => Idea) private ideas;
+    // keeps track of users that have already rated an idea
+    mapping(uint => mapping(address => bool)) private rated;
 
-    function addIdea(
-        string memory _name,
-        string memory _description
-    ) public {
-        uint _perfect = 0;
-        uint _good = 0;
-        uint _bad = 0;
+    /// @dev ensures caller is not the idea's creator/owner
+    modifier notCreator(uint _index) {
+        require(msg.sender != ideas[_index].owner, "Can't rate your own idea");
+        _;
+    }
+
+    /**
+     * @dev allow users to create an idea to receive feedback and support/critics from other users
+     * @param _name is the name of the idea
+     * @param _description is the text explaining the idea
+     */
+    function addIdea(string calldata _name, string calldata _description)
+        external
+    {
+        require(bytes(_name).length > 0, "Empty name");
+        require(bytes(_description).length > 0, "Empty description");
+        uint _rate = 0;
         uint _noOfsupports = 0;
+        uint _donationAmount = 0;
         ideas[ideasLength] = Idea(
             payable(msg.sender),
             _name,
             _description,
-            _perfect,
-            _good,
-            _bad,
-            _noOfsupports
+            _rate,
+            _noOfsupports,
+            _donationAmount
         );
         ideasLength++;
     }
 
-    function getIdea(uint _index) public view returns (
-        address payable,
-        string memory, 
-        string memory,  
-        uint,
-        uint,  
-        uint,
-        uint
-    ) {
+    /**
+     * @return data of an Idea
+     */
+    function getIdea(uint _index)
+        public
+        view
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            uint,
+            uint,
+            uint
+        )
+    {
         return (
             ideas[_index].owner,
-            ideas[_index].name,  
-            ideas[_index].description, 
-            ideas[_index].perfect,
-            ideas[_index].good,
-            ideas[_index].bad,
-            ideas[_index].noOfsupports
-           
+            ideas[_index].name,
+            ideas[_index].description,
+            ideas[_index].rate,
+            ideas[_index].noOfsupports,
+            ideas[_index].donationAmount
         );
     }
 
-     function removeIdea(uint _index) external {
-	        require(msg.sender == ideas[_index].owner, "Only the owner can remove idea");         
-            ideas[_index] = ideas[ideasLength - 1];
-            delete ideas[ideasLength - 1];
-            ideasLength--; 
-	 }
-
-      function perfect(uint _index) external {
-	        require(msg.sender != ideas[_index].owner, "Can't rate your own idea");         
-            ideas[_index].perfect++;
-	 }
-      function good(uint _index) external {
-	        require(msg.sender != ideas[_index].owner, "Can't rate your own idea");         
-            ideas[_index].good++;
-	 }
-      function bad(uint _index) external {
-	        require(msg.sender != ideas[_index].owner, "Can't rate your own idea");         
-            ideas[_index].bad++;
-	 }
-
-    function supportIdea(uint _index, uint _ammount) public payable  {
+    /**
+     * @dev allow ideas' owners to remove their ideas
+     * @notice only ideas' owners can remove their ideas
+     */
+    function removeIdea(uint _index) public {
         require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            ideas[_index].owner,
-           _ammount
-          ),
-          "Transfer failed."
+            msg.sender == ideas[_index].owner,
+            "Only the owner can remove idea"
         );
-        ideas[_index].noOfsupports++;
+        ideas[_index] = ideas[ideasLength - 1];
+        delete ideas[ideasLength - 1];
+        ideasLength--;
     }
-    
+
+    /**
+     * @dev allow users to rate an idea within the range of 0-5
+     * @param rate is the choosen rate for caller for idea
+     */
+    function rateIdea(uint _index, uint rate) external notCreator(_index) {
+        require(rate >= 0 && rate <= 5, "Rate has to be between 1 and 5");
+        Idea storage currentIdea = ideas[_index];
+        require(!rated[_index][msg.sender], "You have already rated this idea");
+        uint totalRates = rate + currentIdea.rate;
+        currentIdea.rate = totalRates;
+        currentIdea.noOfsupports++;
+        rated[_index][msg.sender] = true;
+    }
+
+    /// @dev supports an idea by donating @param _amount
+    function supportIdea(uint _index, uint _amount)
+        public
+        payable
+        notCreator(_index)
+    {
+        Idea storage currentIdea = ideas[_index];
+        uint totalAmount = currentIdea.donationAmount + _amount;
+        currentIdea.donationAmount = totalAmount;
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                ideas[_index].owner,
+                _amount
+            ),
+            "Transfer failed."
+        );
+    }
+
     function getIdeasLength() public view returns (uint) {
         return (ideasLength);
     }
