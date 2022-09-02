7bae81b7a8831585f2f3e399c64d2204c4dcd734
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-09-02 18:22:45 +0400

    Additional bug preventions

--- src/contracts/idea.sol
+++ src/contracts/idea.sol
@@ -28,7 +28,7 @@ interface IERC20Token {
 }
 
 contract IdeaTown {
-    uint internal ideasLength = 0;
+    uint256 internal ideasLength = 0;
     address internal cUsdTokenAddress =
         0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
@@ -36,18 +36,25 @@ contract IdeaTown {
         address payable owner;
         string name;
         string description;
-        uint rate;
-        uint noOfsupports;
-        uint donationAmount;
+        uint256 rate;
+        uint256 noOfsupports;
+        uint256 donationAmount;
     }
 
-    mapping(uint => Idea) private ideas;
+    mapping(uint256 => Idea) private ideas;
+
+    mapping(uint256 => bool) private exists;
     // keeps track of users that have already rated an idea
-    mapping(uint => mapping(address => bool)) private rated;
+    mapping(uint256 => mapping(address => bool)) private rated;
 
     /// @dev ensures caller is not the idea's creator/owner
-    modifier notCreator(uint _index) {
+    modifier notCreator(uint256 _index) {
         require(msg.sender != ideas[_index].owner, "Can't rate your own idea");
+        _;
+    }
+    /// @dev checks if idea exists
+    modifier exist(uint256 _index) {
+        require(exists[_index], "Query of nonexistent idea");
         _;
     }
 
@@ -61,9 +68,9 @@ contract IdeaTown {
     {
         require(bytes(_name).length > 0, "Empty name");
         require(bytes(_description).length > 0, "Empty description");
-        uint _rate = 0;
-        uint _noOfsupports = 0;
-        uint _donationAmount = 0;
+        uint256 _rate = 0;
+        uint256 _noOfsupports = 0;
+        uint256 _donationAmount = 0;
         ideas[ideasLength] = Idea(
             payable(msg.sender),
             _name,
@@ -72,22 +79,24 @@ contract IdeaTown {
             _noOfsupports,
             _donationAmount
         );
+        exists[ideasLength] = true;
         ideasLength++;
     }
 
     /**
      * @return data of an Idea
      */
-    function getIdea(uint _index)
+    function getIdea(uint256 _index)
         public
         view
+        exist(_index)
         returns (
             address payable,
             string memory,
             string memory,
-            uint,
-            uint,
-            uint
+            uint256,
+            uint256,
+            uint256
         )
     {
         return (
@@ -104,12 +113,14 @@ contract IdeaTown {
      * @dev allow ideas' owners to remove their ideas
      * @notice only ideas' owners can remove their ideas
      */
-    function removeIdea(uint _index) public {
+    function removeIdea(uint256 _index) public exist(_index) {
         require(
             msg.sender == ideas[_index].owner,
             "Only the owner can remove idea"
         );
         ideas[_index] = ideas[ideasLength - 1];
+        // idea's index is now ideasLength - 1
+        exists[ideasLength - 1] = false;
         delete ideas[ideasLength - 1];
         ideasLength--;
     }
@@ -118,24 +129,29 @@ contract IdeaTown {
      * @dev allow users to rate an idea within the range of 0-5
      * @param rate is the choosen rate for caller for idea
      */
-    function rateIdea(uint _index, uint rate) external notCreator(_index) {
+    function rateIdea(uint256 _index, uint256 rate)
+        external
+        exist(_index)
+        notCreator(_index)
+    {
         require(rate >= 0 && rate <= 5, "Rate has to be between 1 and 5");
         Idea storage currentIdea = ideas[_index];
         require(!rated[_index][msg.sender], "You have already rated this idea");
-        uint totalRates = rate + currentIdea.rate;
+        uint256 totalRates = rate + currentIdea.rate;
         currentIdea.rate = totalRates;
         currentIdea.noOfsupports++;
         rated[_index][msg.sender] = true;
     }
 
     /// @dev supports an idea by donating @param _amount
-    function supportIdea(uint _index, uint _amount)
+    function supportIdea(uint256 _index, uint256 _amount)
         public
         payable
+        exist(_index)
         notCreator(_index)
     {
         Idea storage currentIdea = ideas[_index];
-        uint totalAmount = currentIdea.donationAmount + _amount;
+        uint256 totalAmount = currentIdea.donationAmount + _amount;
         currentIdea.donationAmount = totalAmount;
         require(
             IERC20Token(cUsdTokenAddress).transferFrom(
@@ -147,7 +163,7 @@ contract IdeaTown {
         );
     }
 
-    function getIdeasLength() public view returns (uint) {
+    function getIdeasLength() public view returns (uint256) {
         return (ideasLength);
     }
 }
